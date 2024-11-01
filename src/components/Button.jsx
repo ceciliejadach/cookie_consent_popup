58f4464de180c8741ffa407c852ed686c70b319d
Cecilie <cepe0001@stud.kea.dk>
2024-11-01 10:44:28 +0100

    tilføjet usestate feature

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,9 @@
 const Button = ({ ctatext }) => {
-  return <button className="bg-white dark:bg-black text-black dark:text-white px-8 py-1 rounded-lg">{ctatext}</button>;
+  return (
+    <button className="bg-white dark:bg-black text-black dark:text-white px-8 py-1 rounded-lg" onClick={() => setCookieAccepted(!isCookieAccepted)}>
+      {ctatext}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/CookieConsentPopup.jsx
+++ src/components/CookieConsentPopup.jsx
@@ -1,20 +1,41 @@
+"use client";
 import { LiaCookieBiteSolid } from "react-icons/lia";
 import { RxCross2 } from "react-icons/rx";
-import Button from "@/components/Button";
+
+import { useState } from "react";
 
 const CookieConsentPopup = () => {
+  const [isCookieAccepted, setCookieAccepted] = useState(false);
+  const [isCookieNotAccepted, setCookieNotAccepted] = useState(false);
+
   return (
-    <section className="grid max-w-64 grid-rows-2 px-5 py-5 bg-black dark:bg-white rounded-2xl h-72 self-end">
-      <div className="flex justify-between">
-        <LiaCookieBiteSolid className="text-white dark:text-black w-[3rem] h-[3rem]" />
-        {/* style={{ width: "3rem", height: "3rem", color: "white", transform: "scaleX(-1)" }} */}
-        <RxCross2 className="cursor-pointer text-white dark:text-black" />
-      </div>
-      <div className="grid gap-4">
-        <p className="text-white dark:text-black">We use cookies to improve your user experience.</p>
-        <Button ctatext="I like Cookies" />
-      </div>
-    </section>
+    !isCookieAccepted &&
+    !isCookieNotAccepted && (
+      <section className="grid max-w-64 grid-rows-2 px-5 py-5 bg-black dark:bg-white rounded-2xl h-72 self-end">
+        <div className="flex justify-between">
+          <LiaCookieBiteSolid className="text-white dark:text-black w-[3rem] h-[3rem]" />
+          <button
+            className="h-fit"
+            onClick={() => {
+              setCookieNotAccepted(!isCookieNotAccepted);
+            }}
+          >
+            <RxCross2 className="cursor-pointer text-white dark:text-black" />
+          </button>
+        </div>
+        <div className="grid gap-4">
+          <p className="text-white dark:text-black">We use cookies to improve your user experience.</p>
+          <button
+            className="bg-white dark:bg-black px-8 py-1 rounded-lg"
+            onClick={() => {
+              setCookieAccepted(!isCookieAccepted);
+            }}
+          >
+            <span className="text-black dark:text-white">I like Cookies!</span>
+          </button>
+        </div>
+      </section>
+    )
   );
 };
 
